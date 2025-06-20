6dbae80c9919b9aad0e09f858342e1f6fc41c02a
skeletus-design <116942223+skeletus-design@users.noreply.github.com>
2025-06-20 15:41:56 +0300

    Создание layout

--- app/src/main/java/com/example/exampleapp/MainActivity.kt
+++ app/src/main/java/com/example/exampleapp/MainActivity.kt
@@ -17,16 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            ExampleAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
